ffc79fc9b1578436b246012ac5a7ba3cc7a27c5c
RyanZheng <rzheng1232@gmail.com>
2024-03-30 23:55:22 -0400

    Update LED.java

--- Crescendo-WorkingAuto-03_26_2024/src/main/java/frc/robot/subsystems/LED.java
+++ Crescendo-WorkingAuto-03_26_2024/src/main/java/frc/robot/subsystems/LED.java
@@ -1,19 +1,35 @@
 package frc.robot.subsystems;
+import java.util.Stack;
+
 import com.ctre.phoenix.CANifier.PWMChannel;
 
 import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LED extends SubsystemBase{
 
     AddressableLED led1;
-    PWM bleh;
+    DigitalOutput light;
+    String state;
+    Stack<String> queue;
     public LED(int id){
         //led1 = new AddressableLED(id);
-        bleh = new PWM(id);
+        light = new DigitalOutput(id);
+        state = "idle";
+        queue = new Stack<>();
     }
-    
+    public void setState(String state){
+        this.state = state;
+        if (state.equals("idle")){
+            light.set(true);
+        }
+        else if(state.equals("")){
+
+        }
+    }
     public void periodic(){
+        setState("idle");
     }
 }
